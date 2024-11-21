87186a932fcf01a09bda74e990f457205549f6cf
Tuchanski <gui.rocha2004@gmail.com>
2024-11-20 21:20:18 -0300

    Add Docs for Interfaces

--- src/models/interfaces/Remunerada.java
+++ src/models/interfaces/Remunerada.java
@@ -1,5 +1,17 @@
 package models.interfaces;
 
+/**
+ * Interface que define um comportamento para contas que podem aplicar taxas de correção ao saldo.
+ * Contas que implementam essa interface devem fornecer uma implementação do método taxaCorrecao.
+ */
+
 public interface Remunerada {
+
+    /**
+     * Aplica uma taxa de correção ao saldo da conta.
+     * O valor do saldo será ajustado com base na porcentagem da taxa fornecida.
+     *
+     * @param taxa Porcentagem da taxa de correção a ser aplicada.
+     */
     void taxaCorrecao(double taxa);
 }
